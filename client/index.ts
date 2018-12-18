c223c75157ccdda46f0a36913cdd79223b4d3273
Richard <richard@stromer.org>
2018-12-18 15:03:08 +0100

    wrap client in class

--- client/index.ts
+++ client/index.ts
@@ -1,16 +1,22 @@
 import WebSocket from 'isomorphic-ws';
 
-const ws = new WebSocket('ws://127.0.0.1:4200');
+class Client {
+  constructor(address: string) {
+    const ws = new WebSocket(address);
 
-ws.on('open', () => {
-  console.log('Websocket connected.');
-  ws.send('hi :wave:');
-});
+    ws.on('open', () => {
+      console.log('Websocket connected.');
+      ws.send('hi :wave:');
+    });
 
-ws.on('error', err => {
-  console.log('Error', err);
-});
+    ws.on('error', err => {
+      console.log('Error', err);
+    });
 
-ws.on('close', (code: number, reason: string) => {
-  console.log('Close', code, reason);
-});
+    ws.on('close', (code: number, reason: string) => {
+      console.log('Close', code, reason);
+    });
+  }
+}
+
+new Client('ws://127.0.0.1:4200');
