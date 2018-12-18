4bf11c689855f30f4bfb0d19a0aa2c32de1726c4
Richard <richard@stromer.org>
2018-12-18 12:11:35 +0100

    handle on connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import WebSocket from 'isomorphic-ws';
 
-const wss = new WebSocket.Server({ port: 4200 });
-console.log('Server started at ws://127.0.0.1:4200');
+class Server {
+  constructor() {
+    const wss = new WebSocket.Server({ port: 4200 });
+
+    wss.on('connection', this.onConnection);
+
+    console.log('Server started at ws://127.0.0.1:4200');
+  }
+
+  onConnection(ws: WebSocket) {
+    console.log('onConnection', (ws as any)._socket.address());
+  }
+}
+
+new Server();
