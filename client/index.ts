6f16e0c4bfd372868319c533da99f377bc21a7ea
Richard <richard@stromer.org>
2018-12-18 14:57:18 +0100

    add some logging to client

--- client/index.ts
+++ client/index.ts
@@ -6,3 +6,11 @@ ws.on('open', () => {
   console.log('Websocket connected.');
   ws.send('hi :wave:');
 });
+
+ws.on('error', err => {
+  console.log('Error', err);
+});
+
+ws.on('close', (code: number, reason: string) => {
+  console.log('Close', code, reason);
+});
