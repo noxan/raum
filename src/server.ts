3149d6b1cab07762544d033ab9929958dbdc694d
Richard <richard@stromer.org>
2018-12-24 11:35:12 +0100

    turn identifier in server from symbol to number to make typing and comparisons work

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { IncomingMessage } from 'http';
 
 export default class Server {
   clientId = 0;
-  clients: any = {};
+  clients: { [key: number]: { ws: WebSocket; name: string } } = {};
 
   constructor() {
     const wss = new WebSocket.Server({ port: 4200 });
@@ -14,26 +14,26 @@ export default class Server {
   }
 
   onConnection(ws: WebSocket, req: IncomingMessage) {
+    const clientId = this.clientId++;
     const client = {
       ws,
-      name: `Client #${this.clientId++}`,
+      name: `Client #${clientId}`,
     };
     console.log(client.name, 'connect', req.connection.remoteAddress);
 
-    const identifier = Symbol(client.name);
-    this.clients[identifier] = client;
+    this.clients[clientId] = client;
 
-    ws.on('message', data => this.onMessage(identifier, data));
-    ws.on('close', (code, reason) => this.onClose(identifier, code, reason));
+    ws.on('message', data => this.onMessage(clientId, data));
+    ws.on('close', (code, reason) => this.onClose(clientId, code, reason));
   }
 
-  onMessage(identifier: symbol, data: WebSocket.Data) {
-    const client = this.clients[identifier];
+  onMessage(clientId: number, data: WebSocket.Data) {
+    const client = this.clients[clientId];
     console.log(client.name, 'message', data);
   }
 
-  onClose(identifier: symbol, code: number, reason: string) {
-    const client = this.clients[identifier];
+  onClose(clientId: number, code: number, reason: string) {
+    const client = this.clients[clientId];
     console.log(client.name, 'close', code, reason);
   }
 }
