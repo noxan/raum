296956fee0de83006d2936679647135217467ce3
Richard <richard@stromer.org>
2018-12-24 15:17:57 +0100

    start bootstrapping switch message method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import WebSocket from 'isomorphic-ws';
 
-import { decodeMessage } from '../shared/protocol';
+import { decodeMessage, Action } from '../shared/protocol';
 import Server from './server';
+
+const switchMessage = ({
+  action,
+  model,
+  data,
+}: {
+  action: Action;
+  model: string;
+  data: Object;
+}) => {
+  console.log(action, model, data);
+};
 
 class RaumServer extends Server {
   onMessage(identifier: number, data: WebSocket.Data) {
@@ -9,12 +21,10 @@ class RaumServer extends Server {
 
     try {
       const message = decodeMessage(data as string);
-      console.log(message);
+      switchMessage(message);
     } catch (err) {
       console.error(err);
     }
-
-    // super.broadcast(identifier, data);
   }
 }
 
