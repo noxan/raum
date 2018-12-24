3350e715e0fed2d0507d6916d6943f6b59773293
Richard <richard@stromer.org>
2018-12-24 12:41:07 +0100

    implement decode message

--- shared/protocol.spec.ts
+++ shared/protocol.spec.ts
@@ -1,6 +1,11 @@
-import { Action, encodeMessage } from './protocol';
+import { Action, encodeMessage, decodeMessage } from './protocol';
 
 test('encodeMessage', () => {
   const message = encodeMessage(Action.INSERT);
+  expect(message).toMatchSnapshot();
+});
+
+test('decodeMessage', () => {
+  const message = decodeMessage('1@{}');
   expect(message).toMatchSnapshot();
 });
--- shared/protocol.ts
+++ shared/protocol.ts
@@ -5,6 +5,20 @@ export enum Action {
   DELETE = 4,
 }
 
-export const encodeMessage = (action: Action, data?: object) => {
+const matchRegex = /^([0-9]+)@(\{.*\})$/;
+
+export const encodeMessage = (action: Action, data: object = {}) => {
   return `${action}@${JSON.stringify(data)}`;
+};
+
+export const decodeMessage = (message: string) => {
+  const match = message.match(matchRegex);
+  if (!match) {
+    throw new Error(`Could not parse message: ${message}`);
+  }
+
+  const action = parseInt(match[1], 10);
+  const data = JSON.parse(match[2]);
+
+  return { action, data };
 };
