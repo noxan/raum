a4d75cd01ae2df3bc39d47185e57bde2590c1465
Richard <richard@stromer.org>
2018-12-24 10:41:24 +0100

    implement store update

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -14,3 +14,23 @@ test('store.insert()', () => {
   store.insert('user', { email: 'richard@stromer.org', firstName: 'Richard' });
   expect(Object.values(store.state)).toMatchSnapshot();
 });
+
+test('store.update()', () => {
+  const store = new Store();
+  store.state = {
+    user: {
+      '94d11a8a-fb8f-43cc-b124-289410da2a54': {
+        _id: '94d11a8a-fb8f-43cc-b124-289410da2a54',
+        email: 'richard@stromer.org',
+        firstName: 'Richard',
+      },
+    },
+  };
+  expect(store.state).toMatchSnapshot();
+
+  store.update('user', '94d11a8a-fb8f-43cc-b124-289410da2a54', {
+    email: 'hello@example.com',
+  });
+
+  expect(Object.values(store.state)).toMatchSnapshot();
+});
--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,14 @@ import uuid from 'uuid';
 export default class Store {
   state: { [key: string]: { [key: string]: {} } } = {};
 
+  update(model: string, id: string, data: object) {
+    if (!this.state[model] || !this.state[model][id]) {
+      throw new Error(`${model} with ${id} does not exist.`);
+    }
+
+    this.state[model][id] = Object.assign({}, this.state[model][id], data);
+  }
+
   insert(model: string, data: object) {
     const id = uuid.v4();
 
