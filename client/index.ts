3c775929ae8b85dd0d6b8054e246b0b4c7ccf5c1
Richard <richard@stromer.org>
2018-12-24 15:08:32 +0100

    try to send insert message to server (crashes due to socket state)

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,7 @@
 import WebSocket from 'isomorphic-ws';
 
 import Socket from './socket';
+import { encodeMessage, Action } from '../shared/protocol';
 
 class Client extends Socket {
   protected onOpen() {
@@ -11,6 +12,13 @@ class Client extends Socket {
   protected onMessage(data: WebSocket.Data) {
     console.log('Message.', data);
   }
+
+  insert(model: string, data: object) {
+    const message = encodeMessage(Action.INSERT, model, data);
+    this.send(message);
+  }
 }
 
-new Client('ws://127.0.0.1:4200');
+const client = new Client('ws://127.0.0.1:4200');
+
+client.insert('user', { email: 'hello@world.com' });
