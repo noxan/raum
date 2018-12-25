83b3fb9d57a8f6208bb517e84b11f2f3a883198b
Richard <richard@stromer.org>
2018-12-25 12:10:30 +0100

    start to implement subscribe to model on client side

--- client/index.ts
+++ client/index.ts
@@ -3,7 +3,19 @@ import WebSocket from 'isomorphic-ws';
 import Socket from './socket';
 import { encodeMessage, Action } from '../shared/protocol';
 
+type ModelListener = (data: Array<any>) => void;
+
 class Client extends Socket {
+  private listeners: { [key: string]: Array<ModelListener> } = {};
+
+  private addListener(model: string, listener: ModelListener) {
+    if (this.listeners[model]) {
+      this.listeners[model].push(listener);
+    } else {
+      this.listeners[model] = [];
+    }
+  }
+
   protected onOpen() {
     console.log('Websocket connected.');
     this.send('hi :wave:');
@@ -17,8 +29,16 @@ class Client extends Socket {
     const message = encodeMessage(Action.INSERT, model, data);
     this.send(message);
   }
+
+  subscribe(model: string, listener: ModelListener) {
+    this.addListener(model, listener);
+  }
 }
 
 const client = new Client('ws://127.0.0.1:4200');
 
 client.insert('user', { email: 'hello@world.com' });
+
+client.subscribe('user', users => {
+  console.log(users);
+});
